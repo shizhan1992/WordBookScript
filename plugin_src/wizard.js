7ed2c9436ec6ad8217b0fa8053f95b76932e2c5f
shizhan1992 <shizhanclass@gmail.com>
2014-06-14 22:54:47 +0800

    some amendment

--- plugin_src/wizard.js
+++ plugin_src/wizard.js
@@ -1,11 +1,39 @@
 /**
  * Created by rushshi on 2014/6/10.
  */
-//function progress(added, total){
-//    topLoader.setProgress(added / total);
-//    topLoader.setValue(added.toString() + '/' + total.toString());
-//}
+$(document).ready(function () {
+    count = 0;
+    done = 0;
+    topLoader = $("#topLoader").percentageLoader({width: 120, height: 120, controllable : true});
+    topLoader.setProgress(0);
+    topLoader.setValue('0');
 
+    $("#test").click(function() {
+        if ($("#bookid").val() == '') {
+            alert("bookid is null");
+            $("#bookname").focus();
+            return;
+        }
+        if ($("#file").val() == '') {
+            alert("file is null");
+            $("#file").focus();
+            return;
+        }
+        var regex = /\.txt$/;
+        if(!regex.test($("#file").val())) {
+            alert("file is not txt");
+            $("#file").focus();
+            return;
+        }
+        loadword();
+    });
+
+    chrome.cookies.getAll({
+        url: "http://www.shanbay.com"
+    }, function (cookies) {
+//        document.getElementById('msg').innerHTML = cookies.length;
+    });
+});
 function loadword() {
     var file = document.getElementById('file').files[0];
     var reader = new FileReader();
@@ -18,10 +46,7 @@ function loadword() {
         if(confirm("确认添加单词书？一共"+strs.length + "个单词")){
             document.getElementById('hint').innerHTML = "请耐心等待几分钟，勿关闭窗口，添加单词中......";
             i = 0;
-            j = 1;
             setTimeout(loop,2000);
-//            topLoader.setProgress(1);
-//            topLoader.setValue(strs.length.toString() + '/' + strs.length.toString());
         }else{
             return;
         }
@@ -31,16 +56,6 @@ function loadword() {
 }
 var loop = function(){
     if(i < strs.length){
-//        var id = createunit(book_id);
-//        if(id == -1) return;
-//        for( ;i < 200*j;i++)
-//        {
-//            addword(id,strs[i].trim()) ;
-//        }
-//        j++;
-//        setTimeout(loop,10000);
-//            topLoader.setProgress(done / strs.length);
-//        topLoader.setValue(done.toString() + '/' + strs.length.toString());
         if(i%200 == 0){
             id = createunit(book_id);
             addword(id,strs[i].trim()) ;
@@ -55,19 +70,12 @@ var loop = function(){
     }
     else{
         topLoader.setProgress(done / strs.length);
-        document.getElementById('hint').innerHTML = "添加完成！一共添加了"+ done + "个单词！" +
+        document.getElementById('hint').innerHTML = "添加完成！一共添加了"+ done + "个单词！\r\n" +
             "（如少于预期个数，可能是有重复单词或者错误拼写单词）";
     }
 
 }
-//var countword = function(total){
-//    if(count < total) {
-//        document.getElementById('hint').innerHTML = "添加单词中......" + count + "/" + total;
-//        setTimeout(countword(total), 1000);
-//    }
-//    else
-//        document.getElementById('hint').innerHTML = "添加完成！一共添加了"+done+"/"+strs.length+"个单词！（如果个数少于总个数，则可能是txt格式问题，或者是有重复单词）";
-//}
+
 function createunit(bookidp){
     var xhr = new XMLHttpRequest();
     var params = "name=unit1&description= &wordbook_id="+bookidp;
@@ -103,53 +111,8 @@ function addword(id, word) {
     xhr1.setRequestHeader("Cookie", chrome.cookies);
     xhr1.setRequestHeader("Content-length", params1.length);
     xhr1.send(params1);
-//    count++;
-//    if(xhr1.status == 200){
-//        done++;
-//    }
+
 }
 
-$(document).ready(function () {
-    count = 0;
-    done = 0;
-    var array;
-    topLoader = $("#topLoader").percentageLoader({width: 120, height: 120, controllable : true});
-    topLoader.setProgress(0);
-    topLoader.setValue('0');
 
-    $("#test").click(function() {
-        if ($("#bookid").val() == '') {
-            alert("bookid is null");
-            $("#bookname").focus();
-            return;
-        }
-        if ($("#file").val() == '') {
-            alert("file is null");
-            $("#file").focus();
-            return;
-        }
-        var regex = /\.txt$/;
-        if(!regex.test($("#file").val())) {
-            alert("file is not txt");
-            $("#file").focus();
-            return;
-        }
-        loadword();
-    });
-
-//    $("#msg").click(
-//        function(){
-//            alert("sssss");
-//            chrome.app.window.create('blank.html', {
-//                id: 'blank'
-//            });
-//        }
-//    );
-
-    chrome.cookies.getAll({
-        url: "http://www.shanbay.com"
-    }, function (cookies) {
-//        document.getElementById('msg').innerHTML = cookies.length;
-    });
-});
 
