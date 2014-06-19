a2fe240d6b603aa9682b28bd07f106f4e3ead9cc
shizhan1992 <shizhanclass@gmail.com>
2014-06-19 22:39:35 +0800

    add two functions to test the restrict of site

--- plugin_src/wizard.js
+++ plugin_src/wizard.js
@@ -44,18 +44,44 @@ function loadword() {
         strs = str.split(/\r?\n/);
         book_id = $("#bookid").val();
         if(confirm("确认添加单词书？一共"+strs.length + "个单词")){
-            document.getElementById('hint').innerHTML = "请耐心等待几分钟，勿关闭窗口，添加单词中......";
+//            document.getElementById('hint').innerHTML = "请耐心等待几分钟，勿关闭窗口，添加单词中......";
             i = 0;
             setTimeout(loop,2000);
+//            iID = setInterval(loop1,1000);
         }else{
+
             return;
         }
     });
     // Read in the file
     reader.readAsText(file);
 }
+
+var loop1 = function(){
+    if(i < strs.length){
+        document.getElementById('hint').innerHTML = "this is "+i+"time";
+        if(i%200 == 0){
+            id = createunit(book_id);
+            addword(id,strs[i].trim()) ;
+            i++;
+//            topLoader.setProgress(done / strs.length);
+//            topLoader.setValue(done.toString() + '/' + strs.length.toString());
+        }
+        addword(id,strs[i].trim());
+        i++;
+//        setTimeout(loop,3000);
+    }
+    else{
+        clearInterval(iID);
+        topLoader.setProgress(done / strs.length);
+        document.getElementById('hint').innerHTML = "添加完成！一共添加了"+ done + "个单词！\r\n" +
+            "（如少于预期个数，可能是有重复单词或者错误拼写单词）";
+    }
+}
+
 var loop = function(){
     if(i < strs.length){
+        document.getElementById('hint').innerHTML = "this is "+i+"time";
         if(i%200 == 0){
             id = createunit(book_id);
             addword(id,strs[i].trim()) ;
@@ -63,17 +89,17 @@ var loop = function(){
             setTimeout(loop,5000);
             topLoader.setProgress(done / strs.length);
             topLoader.setValue(done.toString() + '/' + strs.length.toString());
+            return;
         }
-        addword(id,strs[i].trim()) ;
+        addword(id,strs[i].trim());
         i++;
-        setTimeout(loop,500);
+        setTimeout(loop,Math.random()*750+800);
     }
     else{
         topLoader.setProgress(done / strs.length);
         document.getElementById('hint').innerHTML = "添加完成！一共添加了"+ done + "个单词！\r\n" +
             "（如少于预期个数，可能是有重复单词或者错误拼写单词）";
     }
-
 }
 
 function createunit(bookidp){
@@ -99,19 +125,17 @@ function addword(id, word) {
     var xhr1 = new XMLHttpRequest();
     var params1 = "id="  +id  + "&word=" + word;
 //    document.getElementById("msg").innerHTML = "id="  +array[1] + "&word=" + $('#describe').val();
-    xhr1.open("POST", "http://www.shanbay.com/api/v1/wordlist/vocabulary/", true);
-    xhr1.onreadystatechange = function () {
-    if (xhr1.readyState == 4) {
-            count++;
-            if(xhr1.status == 200)
-                done++;
-        }
-     };
+    xhr1.open("POST", "http://www.shanbay.com/api/v1/wordlist/vocabulary/", false);
     xhr1.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
     xhr1.setRequestHeader("Cookie", chrome.cookies);
     xhr1.setRequestHeader("Content-length", params1.length);
     xhr1.send(params1);
-
+    count++;
+    if(xhr1.status == 200){
+        done++;
+    }else{
+        alert("请检查单词书id是否正确并确保网络正常！");
+    }
 }
 
 
